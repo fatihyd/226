ec5c87600afa138c3101291728f20960fce31c86
fatihyd <yildizafatih@gmail.com>
2024-03-04 23:27:56 +0300

    fix typo

--- example1.cpp
+++ example1.cpp
@@ -59,8 +59,7 @@ nodeType* buildListBackward() {
     while (num != -999) {
         newNode = new nodeType; //create a node
         newNode->info = num; //store the data in newNode
-        newNode->link = first; //put newNode at the beginning
-//of the list
+        newNode->link = first; //put newNode at the beginning of the list
         first = newNode; //update the head pointer of
 //the list, that is, first
         cin >> num; //read the next number
